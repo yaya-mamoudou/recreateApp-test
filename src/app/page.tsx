282ca466ff2386eabd3e731bace4ce5e2f688e3e
Yaya Mamoudou <57509871+yaya-mamoudou@users.noreply.github.com>
2024-10-21 01:23:11 +0100

    setup: font-family and font-sizes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,17 @@ import Image from 'next/image';
 export default function HomePage() {
 	return (
 		<div>
-			<main>
-				<h2 className=''>Hello</h2>
+			<main className='w-full h-screen flex flex-col justify-center items-center'>
+				<h2 className='font-black'>Hello</h2>
+				<h2 className='font-extrabold'>Hello</h2>
+				<h2 className='font-bold'>Hello</h2>
+				<h2 className='font-semibold'>Hello</h2>
+				<h2 className='font-medium'>Hello</h2>
+				<h2 className='font-normal'>Hello</h2>
+				<h2 className='font-light'>Hello</h2>
+				<h2 className='font-extralight'>Hello</h2>
+				<h2 className='font-thin'>Hello</h2>
+				<h2 className=' font-space-mono font-bold'> Space Mono 240k+</h2>
 			</main>
 		</div>
 	);
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,15 +7,26 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        black: "#2b2b2b",
-        'black-secondary': "#3b3b3b",
-        'gray-caption': "#858584",
-        'gray-400': "#cccccc",
-        'purple': "#a259ff",
-        'white': "#fff",
-      },
+    colors: {
+      black: "#2b2b2b",
+      'black-secondary': "#3b3b3b",
+      'gray-caption': "#858584",
+      'gray-400': "#cccccc",
+      'purple': "#a259ff",
+      'white': "#fff",
+    },
+    fontFamily: {
+      'work-sans': ['var(--font-work-sans)', 'sans-serif'],
+      'space-mono': ['var(--font-space-mono)', 'sans-serif']
+    },
+    fontSize: {
+      '3xl': '4.188rem',
+      '2xl': '3.188rem',
+      'xl': '2.375rem',
+      'lg': '1.75rem',
+      'md': '1.375rem',
+      'sm': '1rem',
+      'xs': '0.75rem'
     },
   },
   plugins: [],
