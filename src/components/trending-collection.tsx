d6dd539a8b7a9807404a6bea5c9e4faed558212a
Yaya Mamoudou <57509871+yaya-mamoudou@users.noreply.github.com>
2024-10-23 02:08:48 +0100

    hide some trending collections on smaller devices

--- src/components/trending-collection.tsx
+++ src/components/trending-collection.tsx
@@ -1,14 +1,23 @@
+'use client';
 import { collections } from '@/data/mock-data';
 import React from 'react';
 import CollectionCard from './cards/collection-card';
+import { useMediaQuery } from 'react-responsive';
 
 export default function TrendingCollection() {
+	let data = [...collections];
+	const isTablet = useMediaQuery({ query: '(max-width: 1024px)' });
+	const isMobile = useMediaQuery({ query: '(max-width: 640px)' });
+
+	if (isTablet) data = data.slice(0, 2);
+	if (isMobile) data = data.slice(0, 1);
+
 	return (
 		<div className='container'>
 			<h2 className='text-lg font-semibold'>Trending Collection</h2>
 			<p className='text-md'>Checkout our weekly updated trending collection.</p>
 			<div className='grid grid-cols-12 gap-6 mt-12'>
-				{collections.map((item, index) => (
+				{data.map((item, index) => (
 					<div key={index} className='col-span-12 sm:col-span-6 lg:col-span-4'>
 						<CollectionCard {...item} />
 					</div>
